a8211a8fddf4eff4e560bcddbeef45a320b9c398
Kayan Reid <kreid@Zipcoders-MBP.fios-router.home>
2024-11-10 13:21:26 -0500

    Added 5 additional different fields to the Person class. Added setters and getters for these additional filelds.

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -4,10 +4,17 @@ package com.zipcodewilmington.person;
  * If you did ObjectsOne lab, you might recognize this code?
  */
 public class Person {
-    private String name = "";
-    private int age = Integer.MAX_VALUE;
+    private String name;
+    private int age;
+    private double height;
+    private double weight;
+    private String gender;
+    private String email;
+    private String address;
 
     public Person() {
+        this.name = "";
+        this.age = Integer.MAX_VALUE;
     }
 
     public Person(int age) {
@@ -32,10 +39,50 @@ public class Person {
     }
 
     public String getName() {
-        return this.name;
+        return name;
     }
 
     public Integer getAge() {
-        return this.age;
+        return age;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 }
